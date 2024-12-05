69edd03004f2ce7c0436ac370ac551d4b761f15e
IlonaWishec <ilonawishec.management@gmail.com>
2024-12-06 00:59:38 +0300

    module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -4,12 +4,13 @@ import time
 directory = os.getcwd()
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(directory)
-        filetime = os.path.getmtime(directory)
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime('%d.%m.%Y %H:%M', time.localtime(filetime))
-        file_size = os.path.getsize(directory)
-        parent_dir = os.path.dirname(directory)
+        file_size = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
 print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт, Время изменения: {formatted_time}, '
       f'Родительская директория: {parent_dir}')
-# Обнаружен файл: test_file.txt, Путь: C:\Users\user\PycharmProjects\pythonProject\Файлы, Размер: 4096 байт,
-# Время изменения: 02.12.2024 23:21, Родительская директория: C:\Users\user\PycharmProjects\pythonProject
+# Обнаружен файл: test_file.txt, Путь: C:\Users\user\PycharmProjects\pythonProject\Файлы\test_file.txt, 
+# Размер: 169 байт, Время изменения: 01.12.2024 20:39, 
+# Родительская директория: C:\Users\user\PycharmProjects\pythonProject\Файлы
